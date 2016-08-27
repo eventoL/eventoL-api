97a6539fa272e8b4258d96e1dee15d1e668423b3
FedeG <federicog@exo.com.ar>
2016-08-27 03:42:20 -0300

    Change examples file for use load_db

--- examples.js
+++ examples.js
@@ -1,44 +1,6 @@
 'use strict';
 
 const logger = require('./lib/logger');
-const api = require('./lib/model/api');
-api.load();
+const load_db = require('./tests/functional/load-db');
 
-function generateModel(model, docs, callback){
-    docs.forEach((doc) => {
-        model.remove((error) => {
-            if (error) {
-                throw console.log(error);
-            }
-            model.create(doc, (err, result) => {
-                if (err) {
-                    throw console.log(err);
-                }
-                console.log(result);
-                if (callback) {
-                    callback(result);
-                }
-            });
-        });
-    });
-}
-
-let event = {
-    title: 'flisol',
-    description: 'lkasjdlaskjlaskjlaskjlkasjlaskjdaslkj laksdj lksa jlkas nlkn a',
-    owner: '',
-    tags: [],
-    categories: [],
-    location: [-58.381559, -34.603684]
-};
-
-generateModel(api.model.models.Tag, [{name: 'flisol'}, {name: 'software libre'}], (id) => event.tags.push(id));
-generateModel(api.model.models.Category, [{name: 'linux'}], (id) => event.categories.push(id));
-generateModel(api.model.models.User, [{
-    first_name: 'pepe',
-    last_name: 'gomez',
-    username: 'pgomez'
-}], (id) => {
-    event.owner = id;
-});
-setTimeout(() => generateModel(api.model.models.Event, [event]), 3000);
+load_db.load().then(() => logger.info('All documents loader in db'));
